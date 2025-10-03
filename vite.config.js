a3d26966df91b97b5f735ef50392ac6e527091ad
dsd1024 <dsd102495@gmail.com>
2025-10-03 16:03:17 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,16 @@
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { fileURLToPath, URL } from 'node:url'
 
+// https://vitejs.dev/config/
 export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    // 让 "@/xxx" 指向 src 目录
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+  // GitHub Pages 仓库根路径（注意大小写与仓库名一致）
   base: '/HOMI-AI-demo/',
 })
